3383a2cd7350ec227d41c7900f99a5924f6be9cc
ImazuTsuyoshi <ihsoyust0907@icloud.com>
2020-11-26 13:23:25 +0900

    画像追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+
+  mount_uploader :image, ImageUploader
   
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_24_045326) do
+ActiveRecord::Schema.define(version: 2020_11_25_215502) do
 
   create_table "board_tag_relations", force: :cascade do |t|
     t.integer "board_id", null: false
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 2020_11_24_045326) do
     t.string "two"
     t.string "three"
     t.string "characteristic"
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
